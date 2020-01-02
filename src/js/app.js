f1de0d119d9b40f95f93da2007246c079ed015ff
Omar Al-hamad <omar93@hotmail.se>
2020-01-02 02:44:37 +0100

    getting food and addons lists done, figure out this and next week systems

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,5 @@
-//require('./firebaseInit')
+require('./firebaseInit')
 require('./food')
 require('./factory')
 require('./view')
-
-// let week = require('week')
-
-// let date = new Date()
-// let year = date.getFullYear()
-// let month = date.getMonth()
-// let day = date.getDay()
-// let datum = `${year}/${month}/${day}`
-
-// console.log(week())
+require('./getData')
---
+++ src/js/getData.js
@@ -0,0 +1,17 @@
+const firebase = require('firebase')
+const db = firebase.firestore();
+let dbs = ['food', 'addon']
+for (let i = 0; i < 2; i++) {
+    let current = dbs[i]
+    db.collection(current).get().then(snapshot => {
+        snapshot.forEach(doc => {
+            let option = document.createElement('option')
+            if(current === 'food') {
+                option.textContent = doc.data().food
+            } else {
+                option.textContent = doc.data().addon
+            }
+            document.getElementById(current).appendChild(option)
+        })
+    })
+}
