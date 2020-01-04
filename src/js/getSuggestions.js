ce7918f999a943aa4aeeec7acf88899641a97b81
Omar Al-hamad <omar93@hotmail.se>
2020-01-05 00:32:23 +0100

    fixed the undefined problem, shortened code

--- src/js/getSuggestions.js
+++ src/js/getSuggestions.js
@@ -6,32 +6,17 @@ db.collection('suggestions').get().then(snapshot => {
     snapshot.forEach(doc => {
         let map = doc.data()
         for (let key in map) {
-            console.log('--------------------------------'+i) // 0-6, 0-1
             let allFoodRows = document.querySelectorAll('food-row')
-            for(let j = 0; j < allFoodRows.length; j++) {
+            for (let j = 0; j < allFoodRows.length; j++) {
                 let row = allFoodRows[j]
-                console.log('the row active ', row)
-                console.log('DB DATA:', )
-                console.log('key: ', key)
-                console.log('the food:', map[key])
-                
-                let attributeInFoodRow = 'slot'+i
-                console.log('current active attr: ',attributeInFoodRow)
-                let theActiveFoodWeAreLookingAt = map[j]
-                console.log('active food: ',theActiveFoodWeAreLookingAt)
-                let theKeyWithIndex = 'slot'+key
-                console.log('interesting key: ', theKeyWithIndex)
-
-                if(theKeyWithIndex === attributeInFoodRow) {
-                    row.setAttribute(attributeInFoodRow, theActiveFoodWeAreLookingAt)
+                let slotAttr = 'slot' + i
+                let currentFood = map[j]
+                let theKeyWithIndex = 'slot' + key
+                if (theKeyWithIndex === slotAttr) {
+                    if (currentFood === undefined) { currentFood = '' }
+                    row.setAttribute(slotAttr, currentFood)
                 }
-                
-                // if(par1 === par2) {
-                //     row.setAttribute(par1,map[key])
-                //     console.log('par1: ' , par1, ' par2: ', par2)
-                // }
             }
-           
         }
         i++;
     })
